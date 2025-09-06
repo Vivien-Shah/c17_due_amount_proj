4c5c8905c622275747372331e364799780c26be7
Vivien-Shah <hfsthane7787@gmail.com>
2025-09-06 20:30:26 +0530

    Add files via upload

---
+++ c17_due_amount_proj.py
@@ -0,0 +1,16 @@
+'''Program to calculate the money the shopkeeper should pay Vishal back
+
+def dueamount(x,y):
+   
+
+    difference=(x-y)
+
+    return difference
+
+result=dueamount(4,2.50)
+print(result)
+
+print("Change shopkeeper should pay Vishal is $ ",result)'''
+
+# dueamount(4.0,2.50)
+
